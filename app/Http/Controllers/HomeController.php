5bb1d9519f7ca4a8976f052ac18152b8a082523b
AndresAco92 <andres.antonio.aco92@gmail.com>
2021-06-23 15:59:55 +0200

    work...

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
+
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
+        $movies = Movie::all();
+        dump($movies);
         return view('home');
     }
 }
